7b8ceb609a033eece7daa8d22a223fadba9126e0
sema <semaeryilmazx@outlook.com>
2024-02-10 17:15:59 +0300

    added constructor to script.js
    added constructor to script.js

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.sayTitle = function() {
+    this.sayTitle = function () {
         console.log(this.title)
+    };
+    this.sayAuthor = function () {
+        console.log(this.author)
     };
 }
 
@@ -12,5 +15,5 @@ let theLordOfTheRings = new Book('lotr', 'somebody', '457');
 
 theHobbit.sayTitle();
 theLordOfTheRings.sayTitle();
-
-add some stuff in here 
+theHobbit.sayAuthor();
+theLordOfTheRings.sayAuthor();
